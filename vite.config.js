baad01bd8a7cf667baecd3a7c461c0322e09f073
minnu04 <tadikondasrivardhini9@gmail.com>
2026-03-15 14:09:16 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['collage-management-1.onrender.com'],
+    allowedHosts: true,
   },
 });
